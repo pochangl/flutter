9f6172839247f4550ff99faad36a8ad30ec9fbbd
Pochang <pochanglee2008@gmail.com>
2020-08-24 11:14:27 +0800

    add second page and pass data

--- lib/main.dart
+++ lib/main.dart
@@ -4,33 +4,58 @@ void main() {
   runApp(MyApp());
 }
 
+class Data {
+  final String name;
+  final String next;
+  final String data;
+
+  Data({ this.name, this.next, this.data });
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Route Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Page(routeData: Data(name: 'Home Page', next: '/second', data: 'from home')),
+        '/second': (context) => Page(routeData: Data(name: 'Second page', next: '/', data: 'from second')),
+      }
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class Page extends StatelessWidget {
+  Page({Key key, @required this.routeData}) : super(key: key);
 
-  final String title;
+  final Data routeData;
 
   @override
   Widget build(BuildContext context) {
+    String data = ModalRoute.of(context).settings.arguments;
+    
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(routeData.name),
       ),
       body: Center(
-        child: Text('I am home page'),
+        child: Column(
+          children: [
+            Text('I am ${routeData.name} page'),
+            Text('I got $data from previous page'),
+            RaisedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, routeData.next, arguments: routeData.data);
+              },
+              child: Text('Click to go to ${routeData.next}')
+            )
+          ],
+        ),
       ),
     );
   }
