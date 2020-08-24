672071cc674090740496e2bd0e406ef55753f875
Pochang <pochanglee2008@gmail.com>
2020-08-24 14:04:46 +0800

    add: key-value storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,12 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter storage',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter storage'),
     );
   }
 }
@@ -44,22 +45,49 @@ class MyForm extends StatefulWidget {
 class MyFormState extends State<MyForm> {
   final _formKey = GlobalKey<FormState>();
 
+  String fieldValue;
+
+  _loadField() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      fieldValue = prefs.getString('fieldValue') ?? 'Not available';
+    });
+  }
+
+  _saveField(String val) async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      prefs.setString('fieldValue', val);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadField();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
       child: Column(
         children: [
+          Text('Saved text: $fieldValue'),
           TextFormField(
             validator: (value) {
               if (value.isEmpty) return 'Please enter some text';
               return null;
-            }
+            },
+            onChanged: (value) {
+              this.fieldValue = value;
+            },
           ),
           RaisedButton(
             onPressed: () {
               if (_formKey.currentState.validate()) {
-                Scaffold.of(context).showSnackBar(SnackBar(content: Text('Processing Data')));
+                Scaffold.of(context).showSnackBar(SnackBar(content: Text('Saving Data')));
+                _saveField(fieldValue);
               }
             },
             child: Text('Submit'),
